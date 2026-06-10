2ef4768fb75fa188750d08d34ac1954e590e1bec
xboxtrio99 <xboxtrio99@gmail.com>
2026-06-10 09:50:11 +0000

    fix: vendite field in ProductModel

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -6,10 +6,11 @@ class ProductModel {
   final double prezzo;
   final int stock;
   final String immagine;
-  ProductModel({required this.id, required this.nome, required this.descrizione, required this.marca, required this.prezzo, required this.stock, required this.immagine});
+  final int? vendite;
+  ProductModel({required this.id, required this.nome, required this.descrizione, required this.marca, required this.prezzo, required this.stock, required this.immagine, this.vendite});
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
     id: json['id'], nome: json['nome'] ?? '', descrizione: json['descrizione'] ?? '',
     marca: json['marca'] ?? '', prezzo: (json['prezzo'] ?? 0).toDouble(),
-    stock: json['stock'] ?? 0, immagine: json['immagine'] ?? '',
+    stock: json['stock'] ?? 0, immagine: json['immagine'] ?? '', vendite: json['vendite'] as int?,
   );
 }
