1c234fdd9069535e55e39214795c6fce026fb7d3
xboxtrio99 <xboxtrio99@gmail.com>
2026-06-23 10:15:07 +0000

    feat: passaggio a Stripe LIVE - chiavi reali e Google Pay produzione

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -5,7 +5,7 @@ class AppConfig {
       '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoamNxeGpzcHdlZHFpaGpqa2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1OTAwMjMsImV4cCI6MjA5NjE2NjAyM30'
       '.XLebw0DH33-HFhkPOwnBg7v06sBTl_uQ6uistj5Sg6s';
   static const stripePublishableKey =
-      'pk_test_51S3PnZ2HVMlh4j78UQRBj4UW759TjIzV2icwrFrO08hZqbGqP4wk6rtW4bmk7ovW0lLpI3dRwyspaYoiX65FFq8C00K7WBwa0T';
+      'pk_live_51OFYuBDRt4r2dRZcaUAVDi5M6Qw3zrJWp4FOBh1R543oASQ4JRzH9vWrrZg6y6p4lTDQ1QemnQR2OCrbLaZhJfwV00XmEncBYJ';
   static const shopPhone = '081 341 7717';
   static const shopPhoneInternational = '390813417717';
   static const shopStreet = 'Via Nazionale 68';
--- lib/services/stripe_service.dart
+++ lib/services/stripe_service.dart
@@ -56,7 +56,7 @@ class StripeService {
           googlePay: const PaymentSheetGooglePay(
             merchantCountryCode: 'IT',
             currencyCode: 'EUR',
-            testEnv: true,
+            testEnv: false,
           ),
           appearance: const PaymentSheetAppearance(
             colors: PaymentSheetAppearanceColors(primary: Color(0xFF0288D1)),
