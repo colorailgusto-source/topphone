ca0c6d510a8f92df6e9e276b80c5f3fed919a1e4
xboxtrio99 <xboxtrio99@gmail.com>
2026-06-24 22:25:25 +0000

    test: rimesso Google Pay temporaneamente per prova

--- lib/services/stripe_service.dart
+++ lib/services/stripe_service.dart
@@ -55,6 +55,11 @@ class StripeService {
           paymentIntentClientSecret: clientSecret,
           merchantDisplayName: 'Top Phone Torre',
           style: ThemeMode.light,
+          googlePay: const PaymentSheetGooglePay(
+            merchantCountryCode: 'IT',
+            currencyCode: 'EUR',
+            testEnv: false,
+          ),
           appearance: const PaymentSheetAppearance(
             colors: PaymentSheetAppearanceColors(primary: Color(0xFF0288D1)),
           ),
