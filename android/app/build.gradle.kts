66167f87b5cf4dcbdf3a30eb86fe7ec3e0777bc0
xboxtrio99 <xboxtrio99@gmail.com>
2026-06-26 22:34:58 +0000

    fix: password keystore spostata in key.properties (non versionato) invece di scritta in chiaro nel codice

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -39,10 +48,10 @@ dependencies {
 android {
     signingConfigs {
         create("release") {
-            keyAlias = "topphone"
-            keyPassword = "TopPhone2024!"
-            storeFile = file("topphone.keystore")
-            storePassword = "TopPhone2024!"
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
     buildTypes {
